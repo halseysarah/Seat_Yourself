daa190991d3e08bee5a94d647aa770892780c527
SARAH <HALSEYSARAH@Gmail.com>
2014-09-08 15:09:11 -0400

    Index, Show, Edit, and New are working for the Restaurants model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+Restaurant.create!(
+:name => "Lee",
+:category_id => "1",
+:address => "650 King st. West",
+:phone_number => "123-456-7890",
+:capacity => "100",
+:summary => "This is a great restaurant!",
+:image => "Image will go here",
+)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
